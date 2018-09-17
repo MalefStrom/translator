3d0cae9aa532739b54dc92249776001a11704947
Kosov Viktor <mulved@mail.ru>
2018-09-17 09:52:59 +0300

    Add hello.txt

---
+++ src/components/MainPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container-fluid">
+    <div class="row bg-dark p-2">
+      <a href="#" class="navbar-brand text-white">
+        Новости генетики
+      </a>
+    </div>
+    <div class="row">
+      <div class="col-2 bg-info text-white text-center p-2">
+        <h4>Категории</h4>
+      </div>
+    </div>
+  </div>
+</template>
